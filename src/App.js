5f4db3a19ee22d43950d922c51b9b963e8193050
vijaykumar-13 <sappavijaykumar4@gmail.com>
2024-03-28 05:52:32 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import HistorySearch from './components/HistorySearch'
+
+import './App.css'
+
+// These are the list used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <HistorySearch />
+
+export default App
